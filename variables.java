6c03cf993d4aaaeb74c1fcbf135759323ca6215a
satyampyasi <satyampyasi565@gmail.com>
2025-02-17 16:47:47 +0530

    new work

---
+++ variables.java
@@ -0,0 +1,19 @@
+public class variables {
+     public static void main(String[] args) {
+          boolean  ispass = true;
+          int count = 15;
+          byte marks =19;
+          float pi = 3.14f;
+          char myLetter = 'A';
+         
+
+          System.out.println("does pass ="+ ispass);
+          System.out.println("number is count="+ count);
+          System.out.println("marks is="+marks);
+          System.out.println("This is a value of pi ="+pi);
+          System.out.println( "This is first letter = "+myLetter);
+
+          
+
+     }
+}
