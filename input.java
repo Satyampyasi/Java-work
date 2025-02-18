227c73be08d795ebbfbc4766e65ad697b69d1883
satyampyasi <satyampyasi565@gmail.com>
2025-02-18 10:51:03 +0530

    new

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class input {
+     public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          // System.out.println("Enter your age");
+          // int age = sc.nextInt();
+          // System.out.println("your age is="+age);
+          System.out.println("Enter tha 1st no");
+          int firstnumber = sc.nextInt();
+          System.out.println("Enter tha 2st no");
+          int secoundnumber = sc.nextInt();
+          int result = firstnumber+ secoundnumber;
+           System.out.println("sum of two numbwe ="+result);
+           sc.close();
+     }
+}
--- variables.java
+++ variables.java
@@ -5,13 +5,15 @@ public class variables {
           byte marks =19;
           float pi = 3.14f;
           char myLetter = 'A';
-         
+          long changevalue = count;//implicit conversion
+          int newpi = (int)pi;
 
           System.out.println("does pass ="+ ispass);
           System.out.println("number is count="+ count);
           System.out.println("marks is="+marks);
           System.out.println("This is a value of pi ="+pi);
           System.out.println( "This is first letter = "+myLetter);
+          System.out.println("changevalue"+changevalue);
 
           
 
