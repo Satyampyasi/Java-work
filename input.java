9976922249072acaddb666b87ee3bafe722dc44d
satyampyasi <satyampyasi565@gmail.com>
2025-03-19 18:05:48 +0530

    work

--- input.java
+++ input.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class input {
      public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
-          // System.out.println("Enter your age");
-          // int age = sc.nextInt();
-          // System.out.println("your age is="+age);
+          System.out.println("Enter your age");
+          int age = sc.nextInt();
+          System.out.println("your age is="+age);
           System.out.println("Enter tha 1st no");
           int firstnumber = sc.nextInt();
           System.out.println("Enter tha 2st no");
