daba84918c09076cc89289f8569612b3822552a3
satyampyasi <satyampyasi565@gmail.com>
2025-03-22 23:52:21 +0530

    new work

---
+++ Testintrange.java
@@ -0,0 +1,6 @@
+public class Testintrange {
+     public static void main(String[] args) {
+          System.out.println("max sized of int variable"+Integer.MAX_VALUE);
+          System.out.println("min size of int variable "+Integer.MIN_VALUE);
+     }
+}
