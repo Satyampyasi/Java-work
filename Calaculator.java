576bc1dfb7e499900b479d5ee356f45cd7bfaa72
satyampyasi <satyampyasi565@gmail.com>
2025-03-23 00:04:26 +0530

    calculation

---
+++ Calaculator.java
@@ -0,0 +1,10 @@
+public class Calaculator {
+     public static void main(String[] args) {
+         int a,b,c,sum;
+         a=10;
+         b=20;
+         c=30;
+      sum =a+b+c;
+      System.out.println(a+"+"+b+"+"+c+": "+sum);
+     }
+}
