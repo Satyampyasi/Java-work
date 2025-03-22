6b0b56cef75e7af698eba36af3713464e9141448
satyampyasi <satyampyasi565@gmail.com>
2025-03-22 23:48:40 +0530

    new work

---
+++ Testshortrange.java
@@ -0,0 +1,7 @@
+public class Testshortrange {
+
+     public static void main(String[] args) {
+          System.out.println("Max value for short type variable"+Short.MAX_VALUE);
+          System.out.println("Max value for short type variable"+Short.MIN_VALUE);
+     }
+}
