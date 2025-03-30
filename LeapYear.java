c560ce10b68fb2d06928b8ee91b33005002ceaba
satyampyasi <satyampyasi565@gmail.com>
2025-03-30 22:26:26 +0530

    new info

---
+++ LeapYear.java
@@ -0,0 +1,20 @@
+public class LeapYear{
+            boolean isleapyear(int year){
+                if( year%400 == 0)return true;
+                else if(year%100 == 0)return false;
+                else if(year%4==0)return true;
+                else return false;  
+      }
+      public static void main(String[] args) {
+          int year =2024;
+           LeapYear f1 = new LeapYear();
+               if(f1.isleapyear(year)){
+
+                System.out.println("Yes=>" +year+ " is a leap year");
+               }
+               else{
+               System.out.println("No=>" +year+" is not a leap year");
+
+               }
+      }
+}
--- YongestFinder.java
+++ YongestFinder.java
@@ -1,9 +1,9 @@
 public class YongestFinder{
      int age;
  
-     // Static method to find the youngest person
+    
      static String findYoungest(int age1, int age2, int age3) {
-         // Compare the ages and return which person is the youngest
+         
          return (age1 < age2)
              ? (age1 < age3 ? "1st person is youngest" : "3rd person is youngest")
              : (age2 < age3 ? "2nd person is youngest" : "3rd person is youngest");
@@ -14,13 +14,13 @@ public class YongestFinder{
          YongestFinder f2 = new YongestFinder();
          YongestFinder f3 = new YongestFinder();
          
-         // Assigning ages to each person
+         
          f1.age = 25;
          f2.age = 15;
          f3.age = 45;
          
-         // Calling the static method to find the youngest
-         System.out.println(findYoungest(f1.age, f2.age, f3.age));
+
+         System.out.println(findYoungest(f1.age, f2.age, f3.age));  
      }
  }
  
