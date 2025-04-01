dba450a6cea7eacedff1a16e13287118e59a2ec8
satyampyasi <satyampyasi565@gmail.com>
2025-04-01 22:49:06 +0530

    new work

---
+++ CubeRoot.java
@@ -0,0 +1,10 @@
+public class CubeRoot {
+     //Wap program to return cbrt tof number 
+
+     public static void main(String[] args) {
+          int num = Integer.parseInt(args[0]);
+          double result = Math.cbrt(num);
+          System.out.println("Square root of "+num+"is = "+result);
+     }
+
+}
---
+++ ShopingMall.java
@@ -0,0 +1,8 @@
+//Wap program to return sqrtof number 
+class ShopingMall{
+     public static void main(String[] args) {
+          int num = Integer.parseInt(args[0]);
+          double result = Math.sqrt(num);
+          System.out.println("Square root of "+num+"is = "+result);
+     }
+}
