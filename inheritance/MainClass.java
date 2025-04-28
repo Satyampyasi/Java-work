98f1307831ff51cc6aecdb8d8bb7efcacb51f40c
satyampyasi <satyampyasi565@gmail.com>
2025-04-28 14:07:43 +0530

    new work

---
+++ inheritance/MainClass.java
@@ -0,0 +1,34 @@
+package inheritance;
+
+class TablePan(){
+ public void startUsingSwitch(){
+ System.out.println("fan started");
+ }
+}
+class AC{
+public void startUsingRemote(){
+System.out.println("Ac started");
+     }
+}
+class Projector{
+     public void startUsingRemote(){
+          System.out.println("projector started");  
+     }
+     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class MainClass {
+     
+}
