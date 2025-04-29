976924fb041c85b35475f21ae2422cfc0c6e276d
satyampyasi <satyampyasi565@gmail.com>
2025-04-29 15:32:20 +0530

    inheritance

--- inheritance/MainClass.java
+++ inheritance/MainClass.java
@@ -1,34 +1,34 @@
-package inheritance;
 
-class TablePan(){
- public void startUsingSwitch(){
- System.out.println("fan started");
- }
+class LibraryBook{
+  String title , author;
+  LibraryBook(String title , String author){
+          this.title=title;
+          this.author=author;
+  }
 }
-class AC{
-public void startUsingRemote(){
-System.out.println("Ac started");
+class book extends LibraryBook{
+   int selfnumber;
+   book( String title, String author,int selfnumber){
+     super(title,author);
+     this.selfnumber=selfnumber;
+   }
+}
+
+class student extends LibraryBook{
+     student( String x ,String y){
+          super(x,y);
      }
 }
-class Projector{
-     public void startUsingRemote(){
-          System.out.println("projector started");  
-     }
-     
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 public class MainClass {
-     
+     public static void main(String[] args){
+book b1 = new book("nahraj","satyam",001);
+student s1 = new student("ram", "ramayan");
+System.out.println("Author:"+b1.author+"\nBook:"+b1.title+"\nSelfno"+b1.selfnumber);
+System.out.println();
+System.out.println("Author:"+s1.author+"\nBook:"+s1.title);
+           
+     }
 }
+
+
---
+++ method_constructor/calculate.java
@@ -0,0 +1,57 @@
+package method_constructor;
+
+import java.util.Scanner;
+
+class discountCalculator{
+     double price ;
+     double percentage;
+     int couponCode;
+public void calculateDiscount(double price, double percentage){
+      System.out.println("You got discount of:" +price*(percentage/100));
+}
+public void calculateDiscount(double price, double percentage, boolean isMember){
+     if(isMember){
+     System.out.println("Member got discount of :" +price*(percentage/100)+0.05);
+     }
+     System.out.println("You got discount of:" +price*(percentage/100));
+}
+public void calculateDiscount(double price, int couponCode){
+System.out.println("You got discount of:" +(price-couponCode));
+}
+}
+
+public class calculate{
+     public static void main(String[] args){
+          Scanner sc= new Scanner(System.in);
+          discountCalculator f1 = new discountCalculator();
+          System.out.println("Enter 1 if you are member \n Enter 2 if you have coupon code \nother wise 3");
+          int choice = sc.nextInt();
+          if(choice==1){
+             System.out.println("Enter price of product");
+             double price = sc.nextDouble();
+             System.out.println("Enter discount % :");
+             double discount = sc.nextDouble();
+             System.out.println("Enter you are menbership is true or false");
+             boolean mem = sc.nextBoolean();
+             f1.calculateDiscount(price,discount,mem);
+          }
+         else if(choice==2){
+               System.out.println("Enter price of product");
+               double price = sc.nextDouble();
+
+               System.out.println("Enter your coupon code no:");
+               int code = sc.nextInt();
+               f1.calculateDiscount(price,code);
+            }
+            else if(choice==3)
+            {
+               System.out.println("Enter price of product");
+               double price = sc.nextDouble();
+
+               System.out.println("Enter discount % :");
+               double discount = sc.nextDouble();
+               f1.calculateDiscount(price,discount);
+            }
+            else System.out.println(" invalid choice");
+     }
+}
