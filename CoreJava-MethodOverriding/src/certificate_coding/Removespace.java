a76998a9873f535fa02cd698538e2ad04678539d
satyampyasi <satyampyasi565@gmail.com>
2025-07-13 23:06:59 +0530

    new change

--- CoreJava-MethodOverriding/src/certificate_coding/Removespace.java
+++ CoreJava-MethodOverriding/src/certificate_coding/Removespace.java
@@ -2,21 +2,21 @@ package certificate_coding;
 
 class Removespace {
 	public static String removeDuplicatesandSpaces(String str) {
-		boolean arr[] = new boolean[256];
-		String newword = "";
-		int i;
-		for ( i = 0; i < str.length(); i++) {
-			for (int j = 1 + i; j < str.length()-i; j++) {
-				if (str.charAt(i) != str.charAt(j)) {
-                       arr[str.charAt(i)]=true;
-				}
-			}
-			 
+		
+		boolean arr[] =new boolean[256];
+		
+		StringBuilder  newword = new StringBuilder();
+		for ( int i = 0; i < str.length(); i++) {
+			   char current = str.charAt(i);
+			   if(current==' ')  continue;
+			   
+			   if(!arr[current]) {
+				   newword.append(current);
+				   arr[current]=true;
+			   }
 		}
 		
-		
-		
-		return newword;
+		return newword.toString();
 	}
 
 	public static void main(String args[]) {
