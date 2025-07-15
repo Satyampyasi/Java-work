84ee51d385b9f1423da49b0dac7d7b2ddcb960dc
satyampyasi <satyampyasi565@gmail.com>
2025-07-15 21:24:34 +0530

    wk

---
+++ CoreJava-MethodOverriding/src/Array_list/Newcode.java
@@ -0,0 +1,25 @@
+package Array_list;
+
+
+	import java.util.ArrayList;
+	import java.util.Arrays;
+	import java.util.List;
+	public class Newcode {
+	public static void main(String[] args) {
+		ArrayList<Integer>list = new ArrayList<>();
+		list.add(10);
+		list.add(20);
+		System.out.println(list.get(1));
+		list.add(1,50);
+
+	   System.out.println(list.size());
+	   
+	   List<String> list2 = Arrays.asList("mom","twe");
+	   System.out.println(list2);
+	   
+	   List<Integer> list3 = List.of(4,5,9,6,7);
+	   
+	   list.addAll(list3);
+	}
+	}
+
