7af61aa8a148c64044ef5cdeca1f42d96cfec0cf
satyampyasi <satyampyasi565@gmail.com>
2025-09-01 17:48:46 +0530

    new adv task

--- Advance_java_coding/src/jdbc_coding/Account_operation.java
+++ Advance_java_coding/src/jdbc_coding/Account_operation.java
@@ -9,7 +9,8 @@ import java.sql.Statement;
 import java.util.Properties;
 import java.util.Scanner;
 
-class Account{
+class Account
+{
 	long account_number;
 	String account_holder;
 	double account_balance;
@@ -47,70 +48,120 @@ class Account{
 	
 }
 
-//class AccountOperation {
-//	
-// 	static Connection con = null;
-//    PreparedStatement sta = null;
-//
-//	  
-//	  String Query=" insert ";
-//	   
-//	  
-//	  try {
-//		  Properties props = new Properties();
-//	      props.load(new FileReader("src/jdbc_coding/driverinfo.properties"));
-//
-//	      String url = props.getProperty("url");
-//	      String user = props.getProperty("user");
-//	      String pwd = props.getProperty("pwd");
-//	      con = DriverManager.getConnection(url, user, pwd);
-//	      PreparedStatement psta = con.prepareStatement(Query);
-//		  
-//	  }
-//	    
-//	  
-//	  catch(Exception e) {
-//		  e.printStackTrace();
-//		  
-//	  }
-//	 
-//	  
-//  
-//    
-//	
-//}
+class AccountOperation {
+	
+ 	static Connection con = null;
+    PreparedStatement sta = null;
+
+	public static boolean addAccountDetails( Account account) {
+		   
+		   String  Query = " Insert into accounts values( ?,?,?)";
+		   
+		   PreparedStatement psta = con.prepareStatement(Query);
+		  
+		  
+		  psta.setLong(1,account.getAccount_number() );
+		  psta.setString(2,account.getAccount_holder() );
+		  psta.setDouble(3,  account.getAccount_balance());
+		  int k = psta.executeUpdate();
+		if(k>0)
+		{
+			return true;
+		}
+		else return false;
+		
+		
+	  
+}
+	
+	public static void transactionOperation(Account account, Scanner sc ) {
+		
+		
+		String Query = " update accounts set ? where  ACC_NO = ? ";
+		System.out.println(" Enter balance to be update: ");
+		double bal= sc.nextDouble();
+		  
+		 PreparedStatement psta = con.prepareStatement(Query);
+		 psta.setDouble(1,  account.getAccount_balance()+bal);
+		  psta.setLong(2,account.getAccount_number() );
+		
+	}
+	  
+
+	
+	   
+	  
+	  try {
+		  Properties props = new Properties();
+	      props.load(new FileReader("src/jdbc_coding/driverinfo.properties"));
+
+	      String url = props.getProperty("url");
+	      String user = props.getProperty("user");
+	      String pwd = props.getProperty("pwd");
+	      con = DriverManager.getConnection(url, user, pwd);
+	     
+		  
+	  }
+	    
+	  
+	  catch(Exception e) {
+		  e.printStackTrace();
+		  
+	  }
+	 
+	  
+  
+    
+	
+}
 
 
 
-
-
-
-
-
-
-
-
-//
-//public class Account_operation {
-//	
-//	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println(" Enter your Choice: ");
-//		
-//		
-//		
-//		int choice = sc.nextInt();
-//		 switch(choice){
-//			 
-//			 case 1:{ System.out.println(" Enter your Account number:");
-//			   
-//			 
-//			 }
-//			 
+public class Account_operation {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Enter your Choice:\n1.Deposit\n2.Withdraw\n3.Create New Account\n4.View All Account Details\n5.Exit ");
+		
+		
+		
+		int choice = Integer.parseInt(sc.nextLine());
+		 switch(choice){
 			 
+			 case 1: System.out.println(" Enter your Account number:");
+			 Account ac = new Account();
+			 AccountOperation.transactionOperation(ac);
+			 
+			   
+			 break;
+			 case 2:
+				 break;
+			 case 3:
+				 System.out.println("Enter Acount No ");
+				 long accno = sc.nextLong();
+				 sc.nextLine();
+				 System.out.println(" Enter name : ");
+				 String name = sc.nextLine();
+				 
+				 System.out.println("Enter balance: ");
+				 double bal = sc.nextDouble();
+				 
+				 Account ac = new Account(accno,name,bal);
+				 if(AccountOperation.addAccountDetails(ac))
+				 {
+					 System.out.println("Account created Sucessfully: ");
+				 }
+				 else System.out.println(" Acount not created ");
+				 break;
+			 case 4: 
+			 }
+		
+		
+			 
+	}		 
 		 
 	
-
+}
 
 
 
---
+++ Advance_java_coding/src/jdbc_coding/Mock_data_BatchDemo.java
@@ -0,0 +1,34 @@
+package jdbc_coding;
+import  java.io.*;
+import java.sql.*;
+import java.util.Scanner;
+
+public class Mock_data_BatchDemo {
+public static void main(String[] args) throws FileNotFoundException{
+	final String url= "jdbc:oracle:thin:@localhost:1521:XE";
+	final String username ="mydb";
+	final String pass="8220";
+	String Query="Insert Into  StudentInfo values(?,?,?,?)";
+	try {
+		FileReader fis = new FileReader("C:\\Users\\ADMIN\\Downloads\\MOCK_DATA.csv");
+
+		Connection con = DriverManager.getConnection(url, username, pass);
+		PreparedStatement psta = con.prepareStatement(Query);
+		Scanner sc = new Scanner(fis);
+		sc.nextLine();
+		while(sc.hasNext()) {
+			String split[] = sc.nextLine().split(",");
+			for(int i =0;i<split.length;i++) {
+				psta.setString(i+1, split[i]);
+			}
+			psta.addBatch();
+		}
+		psta.executeBatch();
+		System.currentTimeMillis();
+	}
+	catch(Exception e) {
+		e.printStackTrace();
+		
+	}
+}
+}
