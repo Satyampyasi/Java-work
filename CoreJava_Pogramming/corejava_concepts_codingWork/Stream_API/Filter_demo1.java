c28bfe4562c48f7858efd6826bbba7e681b324f6
satyampyasi <satyampyasi565@gmail.com>
2025-09-06 15:44:24 +0530

    new work

---
+++ CoreJava_Pogramming/corejava_concepts_codingWork/Stream_API/Filter_demo1.java
@@ -0,0 +1,34 @@
+package Stream_API;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class Filter_demo1 {
+
+	public static void main(String[] args) {
+		List<Integer> listOfnumber = Arrays.asList( 15,12,18,16,14,17,1,2,4,6,7,9);
+		
+		// Without using Stream;
+		List<Integer> newlist = new ArrayList<>();
+		for( Integer num : listOfnumber) {
+			if( num%2==0) {
+				newlist.add(num);
+			}
+		}
+		System.out.println(newlist.toString());
+		 System.out.println("***********************************************************");
+		
+		 listOfnumber.stream().filter(value -> value%2==0).forEach(System.out::print);
+		 
+		 System.out.println("\n***********************************************************");
+		 
+		 listOfnumber.stream().filter(num->num%2!=0).forEach(num->System.out.println("odd :"+num));
+		
+		System.out.println("\n------------------------------------------------------------");
+		// using stream 
+		  List<Integer>  evenlist= listOfnumber.stream()
+		                .filter( num->num%2==0)
+		                      .toList();
+		  System.out.println(evenlist);
+	}
+
+}
