6b10eeb715f2774fc2b225c786f0ff36caf8180b
satyampyasi <satyampyasi565@gmail.com>
2025-10-03 23:37:34 +0530

    String work

--- CoreJava_Pogramming/corejava_concepts_codingWork/Logical_Question_Stream/Question_2.java
+++ CoreJava_Pogramming/corejava_concepts_codingWork/Logical_Question_Stream/Question_2.java
@@ -1,9 +1,41 @@
 package Logical_Question_Stream;
 
-public class Question_2 {
+import java.util.Scanner;
 
+/*Write a program to print First non-repeated
+character from given String?
+Ex: Software Services Output: o*/
+public class Question_2 {
+	
+	public static String FirstAccourance(String str) {
+		String result="";
+		for(int i =0;i<str.length();i++) {
+			int count=0;
+			if(str.charAt(i)!=' ') {
+				for(int j =0;j<str.length();j++) {
+					if(str.charAt(i)==str.charAt(j)&& i!=j) {
+						count++;
+						break;
+					}
+				}
+				if(count==0) {
+					result+=str.charAt(i);
+					break;
+				}
+				
+			}
+		}
+		
+		return result;
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		  System.out.println("Enter String:");
+		  String str = sc.nextLine();
+		  System.out.println("First non-repeated character is : "+FirstAccourance(str));
+		
 
 	}
 
